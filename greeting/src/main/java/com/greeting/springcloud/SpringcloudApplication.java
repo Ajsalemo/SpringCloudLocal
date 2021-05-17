10bf55fa491c772c8f092b6444e9a0c34d4479fb
Anthony Salemo <ajssalemo@gmail.com>
2021-05-16 21:04:13 -0400

    added spring-cloud dependencies to 'greeting' service

--- greeting/src/main/java/com/greeting/springcloud/SpringcloudApplication.java
+++ greeting/src/main/java/com/greeting/springcloud/SpringcloudApplication.java
@@ -5,13 +5,24 @@ import com.greeting.springcloud.Properties.GreetingProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GreetingProperties.class)
+@EnableDiscoveryClient
 public class SpringcloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudApplication.class, args);
 	}
+
+	// Zipkin/Brave sampling
+	// @Bean
+	// public Sampler getSampler() {
+	// 	return new Sampler();
+	// }
 
 }

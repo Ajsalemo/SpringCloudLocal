2a0f89d992758cd6ed4d3b26dd5ad31dd9c77b3d
Anthony Salemo <ajssalemo@gmail.com>
2021-05-17 19:25:24 -0400

    added a logger to the home  controller

--- greeting/src/main/java/com/greeting/springcloud/Controllers/HomeController.java
+++ greeting/src/main/java/com/greeting/springcloud/Controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.greeting.springcloud.Controllers;
 
+import java.util.logging.Logger;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     String message = "Hello from Spring Cloud - Greeting Service";
+    private static final Logger LOG = Logger.getLogger(GreetingController.class.getName());
 
     @GetMapping("/")
     public String home() {
+        LOG.info("Greeting: " + message);
         return message;
     }
 }

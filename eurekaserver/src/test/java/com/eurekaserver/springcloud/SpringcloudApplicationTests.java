c35bd4414caab6bf8c1576f144d064af741898e3
Anthony Salemo <ajssalemo@gmail.com>
2021-05-18 21:02:28 -0400

    adding the eurkaserver configuration

---
+++ eurekaserver/src/test/java/com/eurekaserver/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaserver.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

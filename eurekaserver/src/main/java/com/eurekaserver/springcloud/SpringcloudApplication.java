c1e39840763c60be3159866443006b3520103f9c
Anthony Salemo <ajssalemo@gmail.com>
2021-05-18 21:12:44 -0400

    implementing all services with eureka

--- eurekaserver/src/main/java/com/eurekaserver/springcloud/SpringcloudApplication.java
+++ eurekaserver/src/main/java/com/eurekaserver/springcloud/SpringcloudApplication.java
@@ -2,8 +2,9 @@ package com.eurekaserver.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringcloudApplication {
 
 	public static void main(String[] args) {

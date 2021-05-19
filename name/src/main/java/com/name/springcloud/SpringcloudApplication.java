a5c3a9439f645a055ea5654a6b0620845c9e56c6
Anthony Salemo <ajssalemo@gmail.com>
2021-05-18 20:15:43 -0400

    updated the config related yml to ignore eureka client discovery until the server gets stood up

--- name/src/main/java/com/name/springcloud/SpringcloudApplication.java
+++ name/src/main/java/com/name/springcloud/SpringcloudApplication.java
@@ -5,9 +5,11 @@ import com.name.springcloud.Properties.NameProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties(NameProperties.class)
+@EnableDiscoveryClient
 public class SpringcloudApplication {
 
 	public static void main(String[] args) {

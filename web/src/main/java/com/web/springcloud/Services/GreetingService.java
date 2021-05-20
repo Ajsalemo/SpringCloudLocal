934ad6f0ddb436849826339fbf591043b5a6e0a2
Anthony Salemo <ajssalemo@gmail.com>
2021-05-20 15:51:58 -0400

    replacing hardcoded URLs with service names

--- web/src/main/java/com/web/springcloud/Services/GreetingService.java
+++ web/src/main/java/com/web/springcloud/Services/GreetingService.java
@@ -6,7 +6,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GreetingService {
-    private static final String URL = "http://localhost:9090/greeting";
+    // "greeting" is the name of the service that is being called
+    // This equates to localhost:9090 (at the moment) - Ribbon intercepts this and checks the service name against its currently running URL
+    // This is help made possible by service discovery
+    private static final String URL = "http://greeting/greeting";
 
     @Autowired
     private RestTemplate rest;
--- web/src/main/java/com/web/springcloud/SpringcloudApplication.java
+++ web/src/main/java/com/web/springcloud/SpringcloudApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +20,7 @@ public class SpringcloudApplication {
 	@Configuration
 	static class MyConfig {
 		@Bean
+		@LoadBalanced
 		public RestTemplate rest() {
 			return new RestTemplateBuilder().build();
 		}

3d4ba5a32a2eb70a65ce06c7772ce22ed22a66f4
Anthony Salemo <ajssalemo@gmail.com>
2021-05-23 15:14:49 -0400

    enabled ZuluProxy on the web service

--- web/src/main/java/com/web/springcloud/SpringcloudApplication.java
+++ web/src/main/java/com/web/springcloud/SpringcloudApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableZuulProxy
 public class SpringcloudApplication {
 
 	public static void main(String[] args) {
